672f408ab90c5adf3425118233fbcf904a7dcee6
MAHENDRA <mchaudhari767@gmail.com>
2021-09-29 15:44:00 +0530

    By MAAC

---
+++ src/test/java/utility/Demo.java
@@ -0,0 +1,10 @@
+package utility;
+
+public class Demo
+{
+	public static void main(String[]args)
+{
+ System.out.println("Print first Git Project pull By : MAAC")
+ System.out.println("Print first Git Project push By : MAAC")
+}
+}
